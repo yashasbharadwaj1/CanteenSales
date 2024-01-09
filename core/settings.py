b54d1c707742c1e9ca22a3445b7c1b01321dfd23
Yashas <yashasbharadwaj6@gmail.com>
2024-01-09 12:00:24 +0530

    db config plus requirements

--- core/settings.py
+++ core/settings.py
@@ -70,15 +70,19 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'core.wsgi.application'
-
+from dotenv import load_dotenv
+load_dotenv()
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD':os.getenv("DB_PASS"),
+        'HOST': os.getenv("DB_HOST"),
     }
 }
 
