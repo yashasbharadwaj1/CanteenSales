9162ed935b84e8400ab32ef7f4eeb457f1c2be27
Yashas <yashasbharadwaj6@gmail.com>
2024-01-09 12:26:27 +0530

    admin improvements

--- core/urls.py
+++ core/urls.py
@@ -26,3 +26,6 @@ urlpatterns = [
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+
--- sales/admin.py
+++ sales/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from .models import Product,Inventory,Sales,Expenditure
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Inventory) 
 admin.site.register(Sales)
 admin.site.register(Expenditure)
 
+admin.site.site_title = "Canteen Administration"
+admin.site.site_header = "Canteen Admin"
+admin.site.index_title = "Administration"
+
+# unregister
+admin.site.unregister(User)
+admin.site.unregister(Group)
