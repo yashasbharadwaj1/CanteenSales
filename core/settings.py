c5804108c19d54e491cdcb1eeebf1bfa81dbfc65
Yashas <yashasbharadwaj6@gmail.com>
2024-01-19 14:46:36 +0530

    simple env separation

--- core/settings.py
+++ core/settings.py
@@ -75,23 +75,24 @@ load_dotenv()
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD':os.getenv("DB_PASS"),
-        'HOST': os.getenv("DB_HOST"),
+environment = os.getenv("ENVIRONMENT")
+if environment == "staging":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.getenv("DB_NAME"),
+            'USER': os.getenv("DB_USER"),
+            'PASSWORD':os.getenv("DB_PASS"),
+            'HOST': os.getenv("DB_HOST"),
+        }
     }
-}
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
